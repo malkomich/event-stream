b27592808c4d6d7f5ce00fd5b301380b4d29b0fb
Juan Carlos González Cabrero <malkomich@gmail.com>
2019-01-13 23:11:59 +0100

    New Event Topic

--- src/main/java/com/github/malkomich/event/stream/common/EventTopic.java
+++ src/main/java/com/github/malkomich/event/stream/common/EventTopic.java
@@ -2,6 +2,7 @@ package com.github.malkomich.event.stream.common;
 
 public enum EventTopic {
     SCRAPPING("topic.scrapping"),
+    ANALYZE("topic.analyze"),
     SIMULATOR("topic.simulator");
 
     private String topic;
